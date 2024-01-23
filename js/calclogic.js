edbae82dfa1338804b64b57301faf8be8c088c92
Alexander <139145315+Zaandros@users.noreply.github.com>
2024-01-23 15:55:12 +0000

    - Added basic functionality to clear button

--- js/calclogic.js
+++ js/calclogic.js
@@ -1,1 +1,13 @@
-alert("test");
+// Query selectors
+const numberDisplay = document.querySelector(".number-display");
+const clear = document.querySelector("#clear");
+
+//event listeners
+
+clear.addEventListener("click", clearData);
+
+//functions
+
+function clearData() {
+    numberDisplay.innerHTML = "";
+}
